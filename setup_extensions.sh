2eebcfdf9e6836e1967a911812f71e65be2083b5
JSGRD22 <73097618+JSGRD22@users.noreply.github.com>
2025-08-10 21:07:45 +0800

    Removed redundant changes

--- setup_extensions.sh
+++ setup_extensions.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 gnome-extensions enable caffeine@patapon.info
-gnome-extensions disable blur-my-shell@aunetx
 gnome-extensions disable dash-to-dock@micxgx.gmail.com
-gnome-extensions disable search-light@icedman.github.com
 
 gnome-extensions enable clipboard-indicator@tudmotu.com
 gnome-extensions enable cloudflare-warp-toggle@khaled.is-a.dev
